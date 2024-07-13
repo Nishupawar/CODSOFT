bbe1e9911b7145e59b6c090de5263ebb2965dfb8
Nishigandha Pawar <108222462+Nishupawar@users.noreply.github.com>
2024-07-14 00:59:54 +0530

    Create AboutMe.js

---
+++ my-portfolio/components/AboutMe.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import '../styles/AboutMe.css';
+function AboutMe() {
+  return (
+    <div className="about-me">
+      <h1>About Me</h1>
+      <h3><span><b>Name :</b> </span>Nishigandha Pawar</h3>
+        <h3><span><b>Date Of Birth:</b> </span>13-08-2001</h3>
+        <h3><span><b>Nationality:</b> </span>Indian</h3>
+        <h3><span><b>Marrital Status:</b> </span>Unmarried</h3>
+        <h3><span><b>Qualification :</b> </span>Btech-CSE</h3>
+        <h3><span><b>Language :</b> </span>Marathi,Hindi,English</h3>
+    </div>
+  );
+}
+
+export default AboutMe;
