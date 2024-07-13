b3cb23eeffde7bc7ad023198399d8f6df4ec4c30
Nishigandha Pawar <108222462+Nishupawar@users.noreply.github.com>
2024-07-14 01:02:24 +0530

    Add files via upload

---
+++ my-portfolio/components/Home.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import '../styles/Home.css';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to My Portfolio</h1>
+      {/* <h3>Hii THERE !!</h3> */}
+      <h2>I'M <span>Nishigandha Pawar</span></h2>
+      {/* <p>Seeking a position in the field of Computer Science where I can 
+        utilize my skills to further work towards personal and professional development 
+        and contribute towards the prosperity of the organization.</p> */}
+    </div>
+  );
+}
+
+export default Home;
---
+++ my-portfolio/components/ProjectShowcase.js
@@ -0,0 +1,29 @@
+
+import React from 'react';
+import '../styles/ProjectShowcase.css';
+
+function ProjectShowcase() {
+  return (
+    <div className="project-showcase">
+      <h1>Projects</h1>
+      <li><b>Cafe Management System</b></li>
+      <p >The project  is design with Apache NetBeans IDE 13 is based on a concept to maintain orders and management of a particular 
+            items.The role of the User is to maintain information including operations like modifying, deleting, updating the items records and customer order 
+            records in the system.  There will be many items available in the cafe, many customers will be coming at different time for having food, they will be 
+            selecting items from the displayed menu and their bills will be generated.  This system will save time and will be easy to use when compared to manual
+             work that was done in paper.</p>
+           
+              <li><b>Weather Prediction Using Machine Learning</b></li>
+           
+           <p>Weather Prediction project is a web-based application where you  will  be  able  to  access  all  reports  related  to  weather forecasts for any 
+            locations. Its  location  detected  by  your  browser  setting  and  server configuration will  automatically identify the  location and be able to 
+             present its  weather information such  as temperature, wind direction, rainfall, humidity etc. To  change  location  you  will  have  to  select  the 
+              options provided   to  get  its  details.  Its  new  avatar  and  feed burner  will  also  allow  its  users  to  receive  weather  reports directly 
+               from  their text message,  where  they have  not been able to access this particular domain even if the server is down.</p>
+       
+              
+    </div>
+  );
+}
+
+export default ProjectShowcase;
